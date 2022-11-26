4dec2d26e0cc1678246bc8be9d908fcbc7c6243d
karankharole <karankharole2000@gmail.com>
2022-11-26 10:25:51 +0530

    calculate the lenght of line

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("======== WELCOME TO LINE COMPARISON PROGRAM ========");
+
+            LineComparison lineComparison = new LineComparison();
+            lineComparison.lenthOfLine();
+
+        }
+    }
+}
